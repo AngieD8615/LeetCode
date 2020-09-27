a5e799bd96e1e6174c99ff5781dd0b87f05316d9
AngieD8615 <adavidson1@gmail.com>
2020-09-27 01:19:52 -0700

    fixed/cleaned up LargestNumber, created to solution functions. Question: time complexity????

--- sept25/LargestNumber.js
+++ sept25/LargestNumber.js
@@ -16,21 +16,30 @@ Output: "9915393323123042"
 Note: The result may be very large, so you need to return a string instead of an integer.
 */
 
+// time to run ~ 190ms 
 var largestNumber = function (nums) {
   var resultStr = '';
   nums.sort().reverse();
-  for (var k = 9; k > 0; k--) {
+  if (nums[0] === 0 && nums[nums.length - 1] === 0) return '0'
+  var i = 0;
+  for (var k = 9; k >= 0; k--) {
     let set = [];
-    for (var i = 0; i < nums.length; i++) {
+    for (i; i < nums.length; i++) {
       if (nums[i] === k || Number(nums[i].toString()[0]) === k) {
         set.push(nums[i])
+      } else {
+        break;
       }
     }
     let resort = true;
     while (resort) {
       resort = false
       for (var j = 0; j < set.length - 1; j++) {
+        if (set[j] === set[j + 1]) {
+          continue;
+        }
         let sorted = sort(set[j], set[j + 1])
+        console.log(sorted)
         if (sorted[0] === set[j + 1] && sorted[1] === set[j]) {
           set[j] = sorted[0];
           set[j+1] = sorted[1];
@@ -44,31 +53,33 @@ var largestNumber = function (nums) {
 
 };
 
+// looks cleaner but slower to run. ~650ms WHY IS THAT????
+// var largestNumber = function (nums) {
+
+//   let resort = true;
+//   while (resort) {
+//     resort = false
+//     for (var i = 0; i < nums.length - 1; i++) {
+//       let sorted = sort(nums[i], nums[i + 1])
+//       if (sorted) {
+//         resort = true;
+//         [nums[i], nums[i + 1]] = sorted;
+//       }
+//     }
+//   }
+//   if (nums[0] === 0 && nums[nums.length - 1] === 0) return '0'
+//   return nums.join('');
+// }
+
+
 sort = (num1, num2) => {
-  let largerVal = (num1 > num2) ? num1 : num2;
-  let smallerVal = (num1 > num2) ? num2 : num1;
-  if (largerVal.toString().length === smallerVal.toString().length) {
-    return [largerVal, smallerVal]
-  } else {
-    for (var i = 0; i < largerVal.toString().length; i++) {
-      if (Number(largerVal.toString()[i]) > Number(smallerVal.toString()[i])) {
-        return [largerVal, smallerVal]
-      }
-      if (!smallerVal.toString()[i + 1]) {
-        return (Number(smallerVal.toString()[i]) > Number(largerVal.toString()[i + 1])) ? [smallerVal, largerVal] : [largerVal, smallerVal]
-      }
-      if (Number(largerVal.toString()[i]) < Number(smallerVal.toString()[i])) {
-        return [smallerVal, largerVal]
-      }
-    }
-  }
+  let num1num2 = Number('' + num1 + num2);
+  let num2num1 = Number('' + num2 + num1);
+  if (num1num2 === num2num1) return undefined;
+  return (num1num2 > num2num1) ? undefined : [num2, num1];
 }
 
 
-// console.log(sort(322, 3042))
-// console.log(sort(222, 2042))
-// console.log(sort(232, 2))
-// console.log(sort(212, 2))
-// console.log(sort(3242, 3))
-
-console.log(largestNumber([322, 3042, 39, 32, 3, 5, 9, 93, 912]))
+// console.log(largestNumber([824,938,1399,5607,6973,5703,9609,4398,8247]) === "9609938824824769735703560743981399")
+// console.log(largestNumber([121, 12]), largestNumber([121, 12]) === "12121")
+console.log(largestNumber([3, 43, 48, 94, 85, 33, 64, 32, 63, 66]), 9485666463)
