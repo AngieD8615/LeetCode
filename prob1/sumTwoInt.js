0cbce94e17c6ac563e3f5af8f670344fe6e5ba20
AngieD8615 <adavidson1@gmail.com>
2021-02-18 18:50:55 -0700

    completed lets brew code problems 1)sum two nums respresented as int 2)transform letters

--- prob1/sumTwoInt.js
+++ prob1/sumTwoInt.js
@@ -1,37 +1,21 @@
 /*
-Given to non neg int num1 and num2  represented as string, return the sum of num1 and num2. 
-example "345" + "15"
+Given to non neg int num1 and num2 represented as string, return the sum of num1 and num2. 
+example "34524356475867970976764534232454657868" + "167578678789075331417898905"
 
 */
 
 // i: 2 strings that represent numbers
-// o: an integer that is the value of num1 + num2
+// o: an string that is the value of num1 + num2
 
 const sumNums = (num1, num2) => {
-  let pointer1 = num1.length - 1
-  let pointer2 = num2.length - 1
-  let result = ""
-  let needToCarry = false;
-  
-  while (pointer1 >= 0 || pointer2 >= 0 ) {
-    let curNum1 = pointer1 >= 0 ? Number(num1[pointer1]) : 0
-    let curNum2 = pointer2 >= 0 ? Number(num2[pointer2]) : 0
-    let current = needToCarry ? curNum1 + curNum2 + 1 : curNum1 + curNum2
-    let currentAsString = current.toString()
-    if (current < 10) {
-      result = currentAsString + result
-      needToCarry = false;
-    } else {
-      needToCarry = true
-      result = currentAsString[1] + result
-    }
-    pointer1--;
-    pointer2--;
-  }
-  if (needToCarry) {
-    result = "1" + result
-  }
-  return result
+  // pointer1 = num1.length - 1
+  // pointer2 = num2.length - 1
+  // manage carry
+  // result = ["4", "5", "6"] 1 -> 2 -> 4 -> 8 -> 16 -> 32 
+  // reverse the result
+
+  // result = newDigit + result "6" + "54"  => "654"  O(n^2)
+
 }
 
-console.log(sumNums("634", "728"))
+console.log(Number.MAX_VALUE)
--- prob1/transformLeter.js
+++ prob1/transformLeter.js
@@ -3,8 +3,50 @@ Given a string S, we can transform every letter individually to upper or lower c
 string will only contain letter and numbers
 */
 
-// "a3s" => [A3s, a3S, a3s, A3S]
+// "a3sDF" => [a3s, A3s, A3S,  a3S]     
+
+
+//                         ""
+//                   /            \
+//                 a                A
+//                |                  |
+//                a3                 A3
+//             /     \               /       \  
+//           a3s     a3S           A3s        A3S    
+//          /   \     /  \        /   \     /  \
+//        a3sd  a3sD a3Sd a3SD  A3sd  A3sD A3Sd A3SD
+
 
 const transform = (S) => {
   
+  // change all chars to lowercase
+  const s = S.toLowerCase();  // a1
+  // result arr
+  let result = [];  // ["a1"]
+  // helper to build the tree -> inputs curStr
+  const helper = (curStr, count) => {  // ("a", 1)  // ("a1", 2)
+    // base case -> if the curStr length is equal to s length 
+    if (curStr.length === s.length){
+      // push the curStr into the result arr
+      result.push(curStr);
+      // stop
+      return;
+    }
+    else {
+    // not at base case yet 
+      // apply helper to curStr + next Char
+      helper(curStr + s[count], count + 1)   // helper("a" ,1)  // helper("a1", 2)
+      // apply helper to curStr + next Char.Upper
+      if(s[count] !== s[count].toUpperCase()) {
+        helper(curStr + s[count].toUpperCase(), count + 1)  //helper("A", 1)  // helper("a1", 2)
+      }
+    }
+  }
+  // invoke helper
+  helper("",0)  //helper("a1", 0)
+  return result
 }
+
+
+
+console.log(transform("A1s4ts3"), 2**4 === transform("A1s4ts3").length)  // -> ["a1", "A1"]
